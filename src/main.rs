c9644b22307de32996139c82e4a83fa3921d1e94
Jeroen Vannevel <jer_vannevel@outlook.com>
2021-12-25 22:31:42 +0000

    read directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use structopt::StructOpt;
+use std::fs::{self};
 
 #[derive(Debug, StructOpt)]
 struct Options {
-    path: String
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Options::from_args();
     println!("{:?}", args);
+
+    for entry in fs::read_dir(args.path)? {
+        let directory = match entry {
+            Ok(content) => { content },
+            Err(error) => panic!("Invalid directory! {}", error)
+        };
+        println!("{:?}", directory);
+    }
+
+    Ok(())
 }
