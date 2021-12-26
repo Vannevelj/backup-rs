da69e28617c280d1e818b3cb613e2df15f847dbd
Jeroen Vannevel <jer_vannevel@outlook.com>
2021-12-26 11:23:19 +0000

    s3 config

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use structopt::StructOpt;
 use std::fs::{self};
+use aws_sdk_s3::Region;
 
 #[derive(Debug, StructOpt)]
 struct Options {
     #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf
+    path: std::path::PathBuf,
+
+    #[structopt(default_value = "eu-west", long)]
+    region: String
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Options::from_args();
-    println!("{:?}", args);
+    //println!("{:?}", args);
+    let region = Region::new(args.region);
+    let aws_config = aws_config::from_env().region(region).load().await;
 
     for entry in fs::read_dir(args.path)? {
         let directory = match entry {
