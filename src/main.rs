26bb5662846ca711d2f2e4fbdc4f36cebcc33dbf
Jeroen Vannevel <jer_vannevel@outlook.com>
2021-12-27 12:54:12 +0000

    upload to aws

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::path::Path;
-use aws_sdk_s3::{Client, Region};
+use async_recursion::async_recursion;
+use aws_sdk_s3::{ByteStream, Client, Region};
 use log::{error, info};
+use shellexpand::{self};
 use std::collections::HashSet;
 use std::fs::{self};
 use std::io::{Error, ErrorKind};
+use std::path::Path;
 use structopt::StructOpt;
-use shellexpand::{self};
 
 #[derive(Debug, StructOpt)]
 struct Options {
@@ -24,14 +25,16 @@ struct Options {
 
 #[tokio::main]
 async fn main() {
-    env_logger::init_from_env(env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"));
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
 
     let args = Options::from_args();
     let region = Region::new(args.region);
     let aws_config = aws_config::from_env().region(region).load().await;
     let client = Client::new(&aws_config);
 
-    let mut files_by_path = match fetch_existing_objects(&args.bucket, client).await {
+    let mut files_by_path = match fetch_existing_objects(&args.bucket, &client).await {
         Ok(files) => files,
         Err(error) => panic!("Failed to fetch objects: {}", error),
     };
@@ -40,7 +43,7 @@ async fn main() {
 
     let root = expand_path(args.path);
     let second = root.clone();
-    match traverse_directories(root, &second, &mut files_by_path) {
+    match traverse_directories(&root, &second, &mut files_by_path, &client, &args.bucket).await {
         Ok(()) => info!("All directories synced"),
         Err(err) => error!("Failed to sync directories: {}", err),
     }
@@ -48,7 +51,7 @@ async fn main() {
 
 async fn fetch_existing_objects(
     bucket: &String,
-    aws_client: Client,
+    aws_client: &Client,
 ) -> Result<HashSet<Vec<String>>, Box<dyn std::error::Error>> {
     let mut files_by_path = HashSet::<Vec<String>>::new();
     let mut next_token: Option<String> = None;
@@ -80,7 +83,8 @@ async fn fetch_existing_objects(
 }
 
 fn expand_path(input: std::path::PathBuf) -> std::path::PathBuf {
-    let expanded_path: String = shellexpand::tilde::<String>(&input.into_os_string().into_string().unwrap()).to_string();
+    let expanded_path: String =
+        shellexpand::tilde::<String>(&input.into_os_string().into_string().unwrap()).to_string();
     return Path::new(&expanded_path).to_owned();
 }
 
@@ -88,20 +92,45 @@ fn split_filename(filename: &str) -> Vec<String> {
     return filename.split("/").map(|s| s.to_string()).collect();
 }
 
-fn traverse_directories(
-    path: std::path::PathBuf,
+#[async_recursion]
+async fn traverse_directories(
+    path: &std::path::PathBuf,
     root: &std::path::PathBuf,
-    existing_files: &mut HashSet<Vec<String>>
+    existing_files: &mut HashSet<Vec<String>>,
+    aws_client: &Client,
+    bucket: &String,
 ) -> Result<(), Box<dyn std::error::Error>> {
     if path.is_file() {
-        let stripped_path = path.strip_prefix(root)?.file_name().unwrap().to_str().unwrap();
+        let stripped_path = path
+            .strip_prefix(root)?
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap();
         let filename_segments = split_filename(stripped_path);
         if !existing_files.contains(&filename_segments) {
             info!("Uploading new file: {}", stripped_path);
             existing_files.insert(filename_segments);
+
+            let file_data = ByteStream::from_path(path).await;
+            match file_data {
+                Ok(data) => {
+                    let upload_response = aws_client
+                        .put_object()
+                        .bucket(bucket)
+                        .key(stripped_path)
+                        .body(data)
+                        .send()
+                        .await?;
+                }
+                Err(err) => {
+                    error!("Failed to upload file {:?}: {}", stripped_path, err);
+                }
+            }
+
             // upload file
         }
-        return Ok(())
+        return Ok(());
     }
 
     for entry in fs::read_dir(path)? {
@@ -118,7 +147,7 @@ fn traverse_directories(
         };
 
         info!("Evaluating {}", directory_name);
-        traverse_directories(directory.path(), root, existing_files)?;
+        traverse_directories(&directory.path(), root, existing_files, aws_client, bucket).await?;
     }
 
     Ok(())
