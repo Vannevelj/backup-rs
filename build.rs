e8d733f7f2506af751ef7da89397490606d1845f
Jeroen Vannevel <jer_vannevel@outlook.com>
2021-12-28 13:35:58 +0000

    note

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
     // Custom build script is required to embed a manifest which enables long path support on Windows
+    // note: doesn't actually seem to work yet
     embed_resource::compile("backup-rs.rc");
 }
